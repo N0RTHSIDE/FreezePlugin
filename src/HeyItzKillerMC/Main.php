37fd3ac68074bf4de87bf6e1efdfb59a7427d102
N0RTHSIDE <christianschaef10@gmail.com>
2023-05-06 20:50:12 -0400

    Initial Commit

---
+++ src/HeyItzKillerMC/Main.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace HeyItzKillerMC;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+    private $config;
+    private $frozenPlayers = [];
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("This command can only be used in-game.");
+            return false;
+        }
+
+        if (count($args) < 1) {
+            $sender->sendMessage("Usage: /" . $command->getName() . " <player>");
+            return false;
+        }
+
+        $target = $this->getServer()->getPlayerExact($args[0]);
+
+        if ($target === null) {
+            $sender->sendMessage("Player not found.");
+            return false;
+        }
+
+        switch ($command->getName()) {
+            case "ss":
+            case "freeze":
+                if ($this->isPlayerFrozen($target)) {
+                    $this->unfreezePlayer($target);
+                    $sender->sendMessage(TextFormat::GREEN . $target->getName() . " has been unfrozen.");
+                } else {
+                    $this->freezePlayer($target);
+                    $sender->sendMessage(TextFormat::GREEN . $target->getName() . " has been frozen.");
+                }
+                $this->broadcastToOppedPlayers(TextFormat::GRAY . TextFormat::ITALIC . "[" . $sender->getName() . ": " . ($this->isPlayerFrozen($target) ? "froze" : "unfroze") . " " . $target->getName() . "]");
+                break;
+            case "unfreeze":
+                if ($this->isPlayerFrozen($target)) {
+                    $this->unfreezePlayer($target);
+                    $sender->sendMessage(TextFormat::GREEN . $target->getName() . " has been unfrozen.");
+                    $this->broadcastToOppedPlayers(TextFormat::GRAY . TextFormat::ITALIC . "[" . $sender->getName() . ": " . "unfroze " . $target->getName() . "]");
+                } else {
+                    $sender->sendMessage(TextFormat::RED . $target->getName() . " is not frozen.");
+                }
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    private function broadcastToOppedPlayers(string $message): void {
+        foreach ($this->getServer()->getOnlinePlayers() as $player) {
+            if ($player->hasPermission("freezeplugin.broadcast")) {
+                $player->sendMessage($message);
+            }
+        }
+    }
+
+    public function onPlayerMove(PlayerMoveEvent $event): void {
+        $player = $event->getPlayer();
+        if ($this->isPlayerFrozen($player)) {
+            $event->setTo($event->getFrom());
+        }
+    }
+
+    public function freezePlayer(Player $player): void {
+        $playerName = $player->getName();
+        $world = $this->config->get("world");
+
+        $worldManager = $this->getServer()->getWorldManager();
+        $level = $worldManager->getWorldByName($world);
+
+        if (!$level) {
+            $this->getLogger()->warning("World '$world' not found. Please check your config.yml");
+            return;
+        }
+
+        $spawn = $level->getSafeSpawn();
+        $player->teleport($spawn);
+        $player->sendTitle("§cYou have been frozen");
+        $taskHandler = $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function () use ($player): void {
+            if (!$player->isOnline()) {
+                $this->unfreezePlayer($player);
+                return;
+            }
+
+            if ($this->isPlayerFrozen($player)) {
+                $player->sendTitle("§cYou have been frozen");
+            }
+        }), 20 * 5);
+
+        $this->frozenPlayers[$playerName] = $taskHandler;
+        $player->getInventory()->clearAll();
+        $player->getArmorInventory()->clearAll();
+    }
+
+    public function unfreezePlayer(Player $player): void {
+        $playerName = $player->getName();
+        if (!isset($this->frozenPlayers[$playerName])) {
+            return;
+        }
+
+        $taskHandler = $this->frozenPlayers[$playerName];
+        $taskHandler->cancel();
+        unset($this->frozenPlayers[$playerName]);
+
+        if ($player->isOnline()) {
+            $player->sendTitle("§aYou have been unfrozen");
+        }
+    }
+
+    public function isPlayerFrozen(Player $player): bool {
+        return isset($this->frozenPlayers[$player->getName()]);
+    }
+}
